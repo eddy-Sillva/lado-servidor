33133a912f5a759d2da0e4620538619d1e9f4da0
Edwin Jesus Silva Escutia <edwin.jesus.silva@gmail.com>
2021-04-12 13:23:29 -0500

    first commit

--- practica4/practica2.php
+++ practica4/practica2.php
@@ -1,5 +1,6 @@
 <?php
 $str="Hola alumnos del tecnm";
 $patron="/hola/i";
-echo preg_match($patron,$str);
+$cant=preg_match_all($patron,$str);
+echo "La expresion: ".$patron." Fue encontrada: ".$cant." Veces"." En la cadena ".$str;
 ?>
